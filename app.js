cfbbda0f5ffe8fc5f262f18b48582793d88a5c13
Jafet San <j.s.b_95@outlook.com>
2026-04-28 11:23:51 -0600

    Prueba npm test añadida

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req,res) =>{ //req es la petición, y res la respuesta de la API
-    res.send('Esta es una respuesta sencilla')
+    res.send('Esta es una prueba sencilla')
 });
 
 //Función que recibe 2 números y los suma

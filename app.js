fff3ddeedeb3c2e3bfbbd9c66be11855e1173ee2
Jafet San <j.s.b_95@outlook.com>
2026-04-28 11:07:26 -0600

    Cambios de librería y API guardados

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.get('/', (req,res) =>{ //req es la petición, y res la respuesta de la API
     res.send('Esta es una respuesta sencilla')
-})
+});
+
+//Función que recibe 2 números y los suma
+app.get('/suma',(req,res) =>{
+    const { a,b } = req.query;
+    const resultado = Number(a) + Number(b);
+    res.json({resultado});
+});
 
 module.exports = app;
